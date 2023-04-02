576ad71359ea5852245637ae496c283efdd723f9
KirillT <billiscreezo228@gmail.com>
2023-04-02 18:19:41 +0300

    Описание: замена unique на атрибуты

--- rtl/miriscv_alu.sv
+++ rtl/miriscv_alu.sv
@@ -27,7 +27,7 @@ module  miriscv_alu
   always_comb begin
         alu_add = $signed(alu_port_a_i) + $signed(alu_op_i[3] ? ~alu_port_b_i : alu_port_b_i) + alu_op_i[3];
 
-        case (alu_op_i[2:0])
+        (* full_case, parallel_case *) case (alu_op_i[2:0])
             ALU_ADD_SUB:    alu_result_o = alu_add;
             ALU_SLL:        alu_result_o = alu_port_a_i << alu_port_b_i[$clog2(XLEN)-1:0];
             ALU_SLT:        alu_result_o = ($signed(alu_port_a_i) < $signed(alu_port_b_i));
@@ -35,16 +35,16 @@ module  miriscv_alu
             ALU_XOR:        alu_result_o = alu_port_a_i ^ alu_port_b_i;
             ALU_SRL_SRA:    alu_result_o = alu_op_i[3] ? (alu_port_a_i >>> alu_port_b_i[$clog2(XLEN)-1:0]) : alu_port_a_i >> alu_port_b_i[$clog2(XLEN)-1:0];
             ALU_OR:         alu_result_o = alu_port_a_i | alu_port_b_i;
-            default:        alu_result_o = alu_port_a_i & alu_port_b_i;
+            ALU_AND:        alu_result_o = alu_port_a_i & alu_port_b_i;
         endcase
 
-        case (alu_op_i[2:0])
+        (* full_case, parallel_case *) case (alu_op_i[2:0])
             ALU_EQ:     alu_branch_des_o = (cmp_a_i == cmp_b_i);
             ALU_NE:     alu_branch_des_o = (cmp_a_i != cmp_b_i);
             ALU_LT:     alu_branch_des_o = ($signed(cmp_a_i)  < $signed(cmp_b_i));
             ALU_GE:     alu_branch_des_o = ($signed(cmp_a_i) >= $signed(cmp_b_i));
             ALU_LTU:    alu_branch_des_o = (cmp_a_i  < cmp_b_i);
-            default:    alu_branch_des_o = (cmp_a_i >= cmp_b_i);
+            ALU_GEU:    alu_branch_des_o = (cmp_a_i >= cmp_b_i);
         endcase
     end
 
