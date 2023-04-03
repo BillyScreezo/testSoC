91d02380d75d2587139aca9d46611b7630b5baa0
KirillT <billiscreezo228@gmail.com>
2023-04-03 11:47:05 +0300

    Описание: исправление ошибки в умножителе, сокращение числа стадий CoreMark: 22.0 (предыдущий 22.0) Worst slack: -8.688 ns (предыдущий -8.841 ns) Total slack: -22608.875 ns (предыдущий -23117.59 ns)

--- rtl/smult_32_32.sv
+++ rtl/smult_32_32.sv
@@ -1,43 +1,41 @@
-module mult_32_32 (
-		input clk,    // Clock
-		input rst_n,  // Asynchronous reset active low
+module smult_32_32 (
+		input 						clk,    // Clock
+		input 						rst_n,  // Asynchronous reset active low
 		
-		input 	logic signed [31:0] ai, bi,
-		output 	logic signed [63:0] r,
+		input 	logic signed [32:0] ai, bi,
+		output 	logic  		 [63:0] r,
 
 		input 	logic 				req,
 		output 	logic 				rdy
 );
 
-	localparam int PIPE_SIZE = 6;
+	localparam int PIPE_SIZE = 4;
 
-	logic [31:0] a, b, au, bu;	// Преобразованные операнды
+	logic [32:0] ma, mb; 	// Отрицание 'a' и 'b'
+	logic [31:0] au, bu;	// Беззнаковые операнды
 
-	logic [31:0] a1_b1;	// Промежуточные произведения
+	logic [31:0] a1_b1;		// Промежуточные произведения
 	logic [31:0] a2_b1;
 	logic [31:0] a1_b2;
 	logic [31:0] a2_b2;
 
-	logic [63:0] a1_b1_f;	// Промежуточные произведения
-	logic [63:0] a2_b1_f;
-	logic [63:0] a1_b2_f;
-	logic [63:0] a2_b2_f;
-
 	logic [63:0] pre_summ_l, pre_summ_r, summ;
 
-	logic [15:0] a1, a2, b1, b2;	// Урезанные части операндов
-
+	logic [15:0] a1, b1, a2, b2; // Урезанные части операндов
 
 	logic sign_a, sign_b;
 
 	logic [$clog2(PIPE_SIZE)-1:0] pipe_cnt;
 
 // Преобразование операндов
-	assign sign_a = ai[31];
-	assign sign_b = bi[31];
+	assign sign_a = ai[32];
+	assign sign_b = bi[32];
 
-	assign au = sign_a ? -ai : ai;
-	assign bu = sign_b ? -bi : bi;
+	assign ma = -ai;
+	assign mb = -bi;
+
+	assign au = sign_a ? ma[31:0] : ai[31:0];	// Получили модули операндов
+	assign bu = sign_b ? mb[31:0] : bi[31:0];
 
 // Деление операндов на части
 	always_ff @(posedge clk) begin
@@ -49,7 +47,6 @@ module mult_32_32 (
 	end
 
 // Промежуточные операции на dsp
-	 
 	always_ff @(posedge clk) begin
 		a1_b1 <= a1 * b1;
 		a2_b1 <= a2 * b1;
@@ -58,22 +55,14 @@ module mult_32_32 (
 	end
 
 	always_ff @(posedge clk) begin
-		a1_b1_f <= a1_b1;
-		a2_b1_f <= a2_b1 << 16;
-		a1_b2_f <= a1_b2 << 16;
-		a2_b2_f <= a2_b2 << 32;
-	end
-
-	always_ff @(posedge clk) begin
-		pre_summ_l <= a1_b1_f + a2_b1_f;
-		pre_summ_r <= a1_b2_f + a2_b2_f;
+		pre_summ_l <= {32'h0, a1_b1}        + {16'h0, a2_b1, 16'h0};
+		pre_summ_r <= {16'h0, a1_b2, 16'h0} + {a2_b2, 32'h0};
 
 		summ <= pre_summ_l + pre_summ_r;
 	end
 
 // Преобразование результата
-	always_ff @(posedge clk)
-		r <= ((sign_a && sign_b) || (!sign_a && !sign_b)) ? summ : -summ;
+	assign r = ((sign_a && sign_b) || (!sign_a && !sign_b)) ? summ : -summ;
 
 // Логика выдачи готовности
 	always_ff @(posedge clk)
@@ -91,4 +80,4 @@ module mult_32_32 (
 		else
 			rdy <= (pipe_cnt == PIPE_SIZE - 1);
 
-endmodule : mult_32_32
+endmodule : smult_32_32
