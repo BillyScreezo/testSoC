0c01f0db8628b2ecb8d3040ce72d66752f30841e
KirillT <billiscreezo228@gmail.com>
2023-04-03 15:28:22 +0300

    Описание: + 1 стадия, алгоритм "быстрого" умножения для 18 битных чисел CoreMark: 23.0 (предыдущий 22.0) Worst slack: -9.058 ns (предыдущий -8.688 ns) Total slack: -22779.484 ns (предыдущий -22608.875 ns)

--- rtl/smult_32_32.sv
+++ rtl/smult_32_32.sv
@@ -9,19 +9,22 @@ module smult_32_32 (
 		output 	logic 				rdy
 );
 
-	localparam int PIPE_SIZE = 4;
+	localparam int PIPE_SIZE = 5;
+
+	logic [63:0] hr, lr;	// Результат умножения полноценный/сокращённый
 
 	logic [32:0] ma, mb; 	// Отрицание 'a' и 'b'
 	logic [31:0] au, bu;	// Беззнаковые операнды
 
-	logic [31:0] a1_b1;		// Промежуточные произведения
+	logic [35:0] a1_b1, a1_b1_lr;		// Промежуточные произведения
 	logic [31:0] a2_b1;
 	logic [31:0] a1_b2;
-	logic [31:0] a2_b2;
+	logic [27:0] a2_b2;
 
 	logic [63:0] pre_summ_l, pre_summ_r, summ;
 
-	logic [15:0] a1, b1, a2, b2; // Урезанные части операндов
+	logic [17:0] a1, b1;
+	logic [13:0] a2, b2; // Урезанные части операндов
 
 	logic sign_a, sign_b;
 
@@ -39,11 +42,11 @@ module smult_32_32 (
 
 // Деление операндов на части
 	always_ff @(posedge clk) begin
-		a1 <= au[15:0];
-		a2 <= au[31:16];
+		a1 <= au[17:0];
+		a2 <= au[31:18];
 
- 		b1 <= bu[15:0];
-		b2 <= bu[31:16];
+ 		b1 <= bu[17:0];
+		b2 <= bu[31:18];
 	end
 
 // Промежуточные операции на dsp
@@ -55,17 +58,17 @@ module smult_32_32 (
 	end
 
 	always_ff @(posedge clk) begin
-		pre_summ_l <= {32'h0, a1_b1}        + {16'h0, a2_b1, 16'h0};
-		pre_summ_r <= {16'h0, a1_b2, 16'h0} + {a2_b2, 32'h0};
+		pre_summ_l <= {a1_b2, 18'h0} + {a2_b1, 18'h0};
+		pre_summ_r <= a1_b1 + {a2_b2, 36'h0};
 
 		summ <= pre_summ_l + pre_summ_r;
 	end
 
 // Преобразование результата
-	assign r = ((sign_a && sign_b) || (!sign_a && !sign_b)) ? summ : -summ;
+	assign hr = ((sign_a && sign_b) || (!sign_a && !sign_b)) ? summ : -summ;
 
 // Логика выдачи готовности
-	always_ff @(posedge clk)
+	always_ff @(posedge clk or negedge rst_n)
 		if(!rst_n)
 			pipe_cnt <= '0;
 		else
@@ -74,10 +77,25 @@ module smult_32_32 (
 			else if(req)
 				pipe_cnt <= pipe_cnt + 1'b1;
 
-	always_ff @(posedge clk)
+	always_ff @(posedge clk or negedge rst_n)
 		if(!rst_n)
 			rdy <= '0;
 		else
-			rdy <= (pipe_cnt == PIPE_SIZE - 1);
+			if((pipe_cnt == 2) && little_mult)
+				rdy <= '1;
+			else
+				rdy <= (pipe_cnt == PIPE_SIZE - 1);
+
+
+	logic little_mult;
+
+	assign little_mult = ~((|au[31:18]) | (|bu[31:18]));
+	assign a1_b1_lr = ((sign_a && sign_b) || (!sign_a && !sign_b)) ? a1_b1 : -a1_b1;
+	assign lr = {{28{a1_b1_lr[35]}}, a1_b1_lr};
+
+	// assign r = little_mult ? lr : hr;
+
+	always_ff @(posedge clk)
+		r <= little_mult ? lr : hr;
 
 endmodule : smult_32_32
