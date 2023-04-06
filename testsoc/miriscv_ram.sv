18783ae3913cf0130acbd5ecb92f23b3fae8478f
BillyScreezo <billiscreezo228@gmail.com>
2023-04-06 17:53:42 +0300

    *Без оптимизаций* Чистка кода, оптимизация RAM

--- testsoc/miriscv_ram.sv
+++ testsoc/miriscv_ram.sv
@@ -16,7 +16,7 @@ module miriscv_ram
     input    logic [31:0]        instr_addr_i,
 
     // data memory interface
-    output   logic               data_rvalid_o,
+    // output   logic               data_rvalid_o,
     output   logic [31:0]        data_rdata_o,
     input    logic               data_req_i,
     input    logic               data_we_i,
@@ -25,13 +25,8 @@ module miriscv_ram
     input    logic [31:0]        data_wdata_i
     );
 
-  genvar 	     i;
-
   logic [31:0] imem [0:RAM_SIZE/4-1];
   logic [31:0] dmem [0:RAM_SIZE/4-1];
-
-  integer iram_index;
-  integer dram_index; 
    
   initial 
     if(IRAM_INIT_FILE != "")    
@@ -43,16 +38,13 @@ module miriscv_ram
 
 
   always_ff @(posedge clk_i) begin
-    instr_rdata_o  <= imem[(instr_addr_i / 4) % RAM_SIZE];
+    instr_rdata_o  <= imem[instr_addr_i[31:2]];
     instr_rvalid_o <= instr_req_i;
   end
 
-
-  
-  
-  always@(posedge clk_i) begin
+  always_ff @(posedge clk_i) begin
       data_rdata_o  <= dmem[data_addr_i[15:2]];
-      data_rvalid_o <= data_req_i;
+      // data_rvalid_o <= data_req_i;
 
       if(data_req_i && data_we_i && data_be_i[0])
         dmem [data_addr_i[15:2]] [7:0]   <= data_wdata_i[7:0];
@@ -62,7 +54,7 @@ module miriscv_ram
         dmem [data_addr_i[15:2]] [23:16] <= data_wdata_i[23:16];
       if(data_req_i && data_we_i && data_be_i[3])
         dmem [data_addr_i[15:2]] [31:24] <= data_wdata_i[31:24];       
-  end // always@ (posedge clk_i)
+  end
    
 
 endmodule
