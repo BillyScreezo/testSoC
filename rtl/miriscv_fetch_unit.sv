347aaed969ecf7a082f4021b91f48eeffe0dc995
BillyScreezo <billiscreezo228@gmail.com>
2023-04-13 14:02:08 +0300

    Описание: конвейерный регистр fetch стадии CoreMark: 37.0 (предыдущий 40.0) Total ticks: 432518 (предыдущий 401420) Worst slack: -8.666 ns (предыдущий -9.578 ns) Total slack: -23377.096 ns (предыдущий -25867.4 ns)

--- rtl/miriscv_fetch_unit.sv
+++ rtl/miriscv_fetch_unit.sv
@@ -35,20 +35,36 @@ module miriscv_fetch_unit
         output  logic             fetch_rvalid_o
     );
 
-    logic [XLEN-1:0] pc_reg, c_pc, n_pc;
+    logic [XLEN-1:0] pc_reg, c_pc, n_pc, c_pc_f, n_pc_f, c_pc_o, n_pc_o;
     logic [XLEN-1:0] pc_next;
     logic [XLEN-1:0] pc_plus_inc;
 
     logic stall_f;
     logic [XLEN-1:0] instr_f, instr;
 
-    assign instr = (stall_f) ? instr_f : instr_rdata_i; // Если был stall - на следующий такт выдаём сохранённую инструкцию
+// ==============================================
+// ===================== Stall logic
+// ==============================================
 
     always_ff @(posedge clk_i)
         if(~arstn_i)
             stall_f <= '0;
         else
             stall_f <= cu_stall_f_i;
+
+    always_ff @(posedge clk_i) begin   // Если был stall - сохраняем инструкцию из регистра памяти для выдачи её на следующий такт
+        instr_f <= (cu_stall_f_i & !stall_f) ? instr_rdata_i : instr_f;
+        c_pc_f  <= (cu_stall_f_i & !stall_f) ? c_pc : c_pc_f;
+        n_pc_f  <= (cu_stall_f_i & !stall_f) ? n_pc : n_pc_f;
+    end
+
+    assign instr  = (stall_f) ? instr_f : instr_rdata_i; // Если был stall - на следующий такт выдаём сохранённую инструкцию и PC
+    assign c_pc_o = (stall_f) ? c_pc_f : c_pc;
+    assign n_pc_o = (stall_f) ? n_pc_f : n_pc;
+
+// ==============================================
+// ===================== PC logic
+// ==============================================
 
     always_ff @(posedge clk_i)
         if ( ~arstn_i )
@@ -58,40 +74,43 @@ module miriscv_fetch_unit
         else if ( !cu_stall_f_i )
             pc_reg <= pc_next;
 
-    always_ff @(posedge clk_i)   // Если был stall - сохраняем инструкцию из регистра памяти для выдачи её на следующий такт
-        instr_f <= (cu_stall_f_i & !stall_f) ? instr_rdata_i : instr_f;
-
     always_ff @(posedge clk_i) begin
         c_pc  <= pc_reg;
         n_pc  <= pc_plus_inc;
     end
 
-    always_ff @(posedge clk_i)
-        if ( ~arstn_i )
-            fetch_rvalid_o  <= '0;
-        else if ( cu_boot_addr_load_en_i )
-            fetch_rvalid_o  <= '0;
-        else
-            if(!fetch_rvalid_o)     // Сбрасываем флаг "неготовности"
-                fetch_rvalid_o <= '1;
-            else if (cu_kill_f_i)   // Выдаём пузырёк, если был переход (1 такт на загрузку адреса)
-                fetch_rvalid_o <= '0;
-
-    assign pc_plus_inc  = pc_reg + 'd4;                                 // PC + 4
-
     always_comb begin
         pc_next = pc_plus_inc;
-        
+
             if(cu_kill_f_i)
                 pc_next = cu_pc_bra_i;
     end
 
-    assign instr_req_o  = ~(cu_stall_f_i);
+    assign pc_plus_inc  = pc_reg + 'd4;
+
+// ==============================================
+// ===================== Pipeline reg
+// ==============================================
+
+    always_ff @(posedge clk_i) begin
+        if(~arstn_i) begin
+            instr_o                 <= { {(XLEN-8){1'b0}}, 8'h13 };
+            fetch_rvalid_o          <= '0;
+            fetched_pc_addr_o       <= '0;
+            fetched_pc_next_addr_o  <= '0;
+        end else if (~cu_stall_f_i )begin
+            instr_o                 <= (cu_kill_f_i | ~instr_rvalid_i) ? { {(XLEN-8){1'b0}}, 8'h13 } : instr;
+            fetch_rvalid_o          <= (cu_kill_f_i | ~instr_rvalid_i) ? '0 : '1;
+            fetched_pc_addr_o       <= c_pc_o;
+            fetched_pc_next_addr_o  <= n_pc_o;
+        end
+    end
+
+// ==============================================
+// ===================== Instr ram 
+// ==============================================
+
+    assign instr_req_o  = ~(cu_kill_f_i);
     assign instr_addr_o = pc_reg;
-
-    assign instr_o      = fetch_rvalid_o ? instr : { {(XLEN-8){1'b0}}, 8'h13 };
-
-    assign fetched_pc_addr_o      = c_pc;
-    assign fetched_pc_next_addr_o = n_pc;
 
 endmodule
